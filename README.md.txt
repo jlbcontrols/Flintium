ec33619526b73541ae8b76af8a0fe5b95f1e2685
jlbcontrols <joseph.burns@jlbcontrols.com>
2020-01-12 13:39:49 -0800

    test commit

--- README.md.txt
+++ README.md.txt
@@ -3,6 +3,7 @@ Ignition with PlantPAX AOIs
 
 -Ignition 8.0.6
 -Studio v32.02
+-PlantPAx_Process_Library_v4.10.01
 -Emulate 32.00
 
 Clone this repo to /Ignition/data/projects
---
+++ jlbFP/ignition/script-python/UdtConversion/code.py
@@ -0,0 +1,37 @@
+#def updateOpcPaths(udtName):
+#	udtPath = "_types_/FactoryPacks/" + udtName
+#	opcStartString = "ns=1;s=[{LeasedPLC}]{PAX Tag}."
+#	return system.udtHelper.updateOpcPath(udtPath, opcStartString)
+#
+#def folderize(udtName):
+#		udtPath = "_types_/FactoryPacks/" + udtName
+#		return system.udtHelper.folderize(udtPath)
+#		
+#def removePrefixes(udtName):
+#		udtPath = "_types_/FactoryPacks/" + udtName
+#		return system.udtHelper.removePrefixes(udtPath)
+#		
+#def updateTagGroup(udtName):
+#		udtPath = "_types_/FactoryPacks/" + udtName
+#		tagGroupString = "FactoryPacks Leased"
+#		return system.udtHelper.updateTagGroup(udtPath, tagGroupString)
+		
+#updateUDT.updateOpcPaths("P_VSA")
+#updateUDT.folderize("P_VSA")
+#updateUDT.removePrefixes("P_VSA")
+#updateUDT.updateTagGroup("P_VSA")
+
+#def convertUdt(udtName):
+#	udtPath = "_types_/FactoryPacks/" + udtName
+#	
+#	system.udtHelper.folderize(udtPath)
+#	
+#	opcStartString = "ns=1;s=[{LeasedPLC}]{PAX Tag}."
+#	system.udtHelper.updateOpcPath(udtPath, opcStartString)
+#	
+#	system.udtHelper.removePrefixes(udtPath)
+#	
+#	tagGroupString = "FactoryPacks Leased"
+#	system.udtHelper.updateTagGroup(udtPath, tagGroupString)
+
+system.udtHelper.
