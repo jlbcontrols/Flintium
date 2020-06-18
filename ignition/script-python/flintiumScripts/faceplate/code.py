cdc16f4bb111bb474fe495ab1ca78f21d77cada6
jlbcontrols <46946127+jlbcontrols@users.noreply.github.com>
2020-06-18 09:18:47 -0700

    Refactor Faceplate Nav (#151)
    * Added titled border containers to main window, minor template cleanup
    
    * partially implemented faceplate nav script refactoring using TemplateManager, still need to handle Main/Alarms and Main/Diagnostics
    
    * Finished refactoring template navigation

--- ignition/script-python/flintiumScripts/faceplate/code.py
+++ ignition/script-python/flintiumScripts/faceplate/code.py
@@ -1,34 +1,72 @@
 ##### Fucntions to open the correct faceplate, depending on UDT of a given instance, and Main or Advanced popup. #####
 
-# Called from vision component scripts
-def open(instancePath, faceplateType):
+# Called from vision component scripts to open a devices faceplate
+def open(event, instancePath, faceplateType):
 
-	print "instancePath=" +instancePath
-	udtTypeName = flintiumScripts.util.getTypeName(instancePath)
-	print udtTypeName
+	udtName = flintiumScripts.util.getTypeName(instancePath)
 	
 	params = {
 		'instancePath':instancePath,
-		'templateFolder':'Flintium/Panels/'+ udtTypeName,
+		'templateFolder':'Flintium/Panels/'+ udtName,
 	}
-	tabProperties = getTabProperties(udtTypeName,faceplateType)
+	tabProperties = getTabProperties(event,udtName,faceplateType)
 	params.update(tabProperties)
 	
 	popupPath = 'Flintium/Faceplates/' + faceplateType
 	window = system.nav.openWindowInstance(popupPath, params)
 	system.nav.centerWindow(window)
 	
-
-def getTabProperties(udtTypeName,faceplateType):
-	udtNav = flintiumScripts.nav.getNavObject(udtTypeName)
+# Return a dictionary of tab data for the faceplate instance. Visible tabs depend on what panel templates exist for a given UDT.
+def getTabProperties(event,udtName,faceplateType):
+	templateManager = event.source.getAppContext().getTemplateManager()
 	if(faceplateType=='Main'):
-		return udtNav.main.getTabDict()
+		return getMainTabDict(udtName, templateManager)
 	if(faceplateType=='Advanced'):
-		return udtNav.advanced.getPageCountDict()
+		return getAdvancedPageCountDict(udtName, templateManager)
 	print "Must select 'Main' or 'Advanced' faceplate type. Received:'" + faceplateType + "'"
 	return {}
 
+# Count all templates in folder with sequential integer names "1", "2" ...
+def getNumberedTemplateCount(folderPath,templateManager):
+	max = 10
+	for i in range(1,max):
+		templatePath = folderPath + "/" + str(i)
+		if not templateExists(templatePath,templateManager):
+			break
+	return i-1
 
+# Check if a template with given path exists in this project
+def templateExists(templatePath,templateManager):
+	if templateManager.getId(templatePath):
+		return True
+	else:
+		return False
 
+# Return the UDT's template folder path
+def getUdtTemplFolderPath(udtName):
+	return "Flintium/Panels/" + udtName
 
+# Dictionary of template page counts for advanced templates
+def getAdvancedPageCountDict(udtName, templateManager):
+	advTemplFolderPath = getUdtTemplFolderPath(udtName)+"/Advanced"
+	return{
+		'maintenancePages':getNumberedTemplateCount(advTemplFolderPath+"/Maintenance",templateManager),
+		'engineeringPages':getNumberedTemplateCount(advTemplFolderPath+"/Engineering",templateManager),
+		'hmiPages':getNumberedTemplateCount(advTemplFolderPath+"/HMI",templateManager),
+		'faultsPages':getNumberedTemplateCount(advTemplFolderPath+"/Faults",templateManager)
+	}
+
+# Find which main templates exist for a particular UDT
+def getMainTabDict(udtName, templateManager):
+	mainTemplFolderPath = getUdtTemplFolderPath(udtName)+"/Main"
+	return{
+		'hasOperator':templateExists(mainTemplFolderPath+"/Operator",templateManager),
+		'hasMaintenance':templateExists(mainTemplFolderPath+"/Maintenance",templateManager),
+		'hasTrends':templateExists(mainTemplFolderPath+"/Trends",templateManager),
+		'hasDiagnostics':templateExists(mainTemplFolderPath+"/Diagnostics",templateManager),
+		'hasAlarms':templateExists(mainTemplFolderPath+"/Alarms",templateManager),
+		'hasAdvanced':sum(getAdvancedPageCountDict(udtName, templateManager).values())>0
+	}
 	
+
+
