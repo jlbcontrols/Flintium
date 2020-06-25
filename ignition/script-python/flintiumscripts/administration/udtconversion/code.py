9b983a9c1ffe7c4ea66b01667a68d01912fb232b
jlbcontrols <joseph.burns@jlbcontrols.com>
2020-06-25 18:03:35 -0400

    updated udtconversion script with new tag group names

--- ignition/script-python/flintiumscripts/administration/udtconversion/code.py
+++ ignition/script-python/flintiumscripts/administration/udtconversion/code.py
@@ -3,37 +3,37 @@
 
 def updateOpcPaths(udtName):
 	import system
-	udtPath = "_types_/Flintium/" + udtName
+	udtPath = "_types_/FlintiumTypes/" + udtName
 	opcStartString = "{opcPrefix}[{plcName}]{plcTag}."
 	return system.udtHelper.updateOpcPath(udtPath, opcStartString)
 	
 def updateOpcServer(udtName):
 	import system
-	udtPath = "_types_/Flintium/" + udtName
+	udtPath = "_types_/FlintiumTypes/" + udtName
 	opcServerString = "{opcServName}"
 	return system.udtHelper.updateOpcServer(udtPath, opcServerString)
 
 def updateHistoryProvider(udtName):
 	import system
-	udtPath = "_types_/Flintium/" + udtName
+	udtPath = "_types_/FlintiumTypes/" + udtName
 	historyProviderString = "{histProvName}"
 	return system.udtHelper.updateHistoryProvider(udtPath, historyProviderString)
 
 def folderize(udtName):
 	import system
-	udtPath = "_types_/Flintium/" + udtName
+	udtPath = "_types_/FlintiumTypes/" + udtName
 	return system.udtHelper.folderize(udtPath)
 		
 def removePrefixes(udtName):
 	import system
-	udtPath = "_types_/Flintium/" + udtName
+	udtPath = "_types_/FlintiumTypes/" + udtName
 	return system.udtHelper.removePrefixes(udtPath)
 		
 def updateTagGroup(udtName):
 	import system
-	udtPath = "_types_/Flintium/" + udtName
-	tagGroupString = "Flintium Leased-Slow"
-	opcPathFilterString = "Cfg_"
+	udtPath = "_types_/FlintiumTypes/" + udtName
+	tagGroupString = "FlintiumLeased"
+	opcPathFilterString = ""  # For expample "Cfg_ would only update tags with Cfg_ in opc path, "" would update all opc tags
 	return system.udtHelper.updateTagGroup(udtPath, tagGroupString, opcPathFilterString)
 
 # Examples:		
@@ -45,7 +45,7 @@ def updateTagGroup(udtName):
 # Use this to apply a function to all the UDT tags.  The function must take udtName as the sole input (i.e. one of the functions listed above)
 # Example: udtconversion.updateAllUdts(udtconversion.updateTagGroup)
 def updateAllUdts(updateFunction):
-	udtList = ["Meta","P_AIn","P_Alarm","P_CmdSrc","P_Gate","P_Intlk","P_Motor","P_MotorRev","P_PIDE","P_ValveC","P_ValveSO","P_VSD","ProgOperKeep"]
+	udtList = ["Meta","P_AIn","P_Alarm","P_CmdSrc","P_Gate","P_Intlk","P_Motor","P_MotorRev","P_Perm","P_PIDE","P_ValveC","P_ValveSO","P_VSD","VariableOwner"]
 	for udtName in udtList:
 		updateFunction(udtName)
 
@@ -53,7 +53,7 @@ def updateAllUdts(updateFunction):
 # Run this to perform intitial conversion, after type is created from OPC drop.  May need to restart module on gateway webpage before use.
 def convertFromOpcDrop(udtName):
 	import system
-	udtPath = "_types_/Flintium/" + udtName
+	udtPath = "_types_/FlintiumTypes/" + udtName
 	
 	folderizedQty = system.udtHelper.folderize(udtPath)
 	
@@ -65,13 +65,9 @@ def convertFromOpcDrop(udtName):
 	
 	prefixUpdatedQty = system.udtHelper.removePrefixes(udtPath)
 	
-	tagGroupString = "Flintium Leased"
+	tagGroupString = "FlintiumLeased"
 	opcPathFilterString = ""
 	tagGroupUpdateAllQty = system.udtHelper.updateTagGroup(udtPath, tagGroupString, opcPathFilterString)
-	
-	tagGroupString = "Flintium Leased-Slow"
-	opcPathFilterString = "Cfg_"
-	tagGroupUpdateCfgQty = system.udtHelper.updateTagGroup(udtPath, tagGroupString, opcPathFilterString)
 	
 	return {
 		'folderizedQty':folderizedQty,
